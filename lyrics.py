00128b56a0d21106dddbb023d342f2408e80b1c8
rikels <knakel2@gmail.com>
2014-12-21 13:55:47 +0100

    Changed pycurl to requests
    I have removed the library pycurl from the script, so you won't need
    that anymore (at least not for this script).

--- lyrics.py
+++ lyrics.py
@@ -1,11 +1,11 @@
 '''
-	* You'll need to download pycurl, xmltodic, BeautifulSoup and maybe json, but i think that is a default one
+	* You'll need to download xmltodic and BeautifulSoup
 	* Easiest method to do so: "(sudo) pip install pycurl xmltodict BeautifulSoup json"
 	
 	* ViewLyrics Open Searcher
 	* Developed by PedroHLC
 	* Converted to python by Rikels
-	* Last update: 5-11-2014
+	* Last update: 21-12-2014
 	
 	* lyricswikia Lyric returner
 	* Developed by Rikels
@@ -13,18 +13,16 @@
 '''
 
 import hashlib
-import pycurl
-import StringIO
-import io
 import httplib, urllib
 import xmltodict
 import json
 from BeautifulSoup import BeautifulSoup
 import re
+import requests
 
 #function to return python workable results from Minilyrics
 def MiniLyrics(artist,title):
-	search_url = "search.crintsoft.com/searchlyrics.htm"
+	search_url = "http://search.crintsoft.com/searchlyrics.htm"
 	search_query_base = "<?xml version='1.0' encoding='utf-8' standalone='yes' ?><searchV1 client=\"ViewLyricsOpenSearcher\" artist=\"{artist}\" title=\"{title}\" OnlyMatched=\"1\" />".encode("utf-8")
 	search_useragent = "MiniLyrics"
 	search_md5watermark = "Mlv1clt4.0"
@@ -67,39 +65,26 @@ def MiniLyrics(artist,title):
 	search_encquery = vl_enc(search_query_base.format(artist = artist, title = title), search_md5watermark)
 
 	def http_post(url, data, ua):
-		#I could set the user agent with Pycurl itself, but since we're already creating a header, I just pasted in here.
-		header = [  "User-Agent: {ua}".format(ua=ua),
-					"Content-Length: {content_length}".format(content_length = len(data)),
-					"Connection: Keep-Alive",
-					"Expect: 100-continue",
-					"Content-Type: application/x-www-form-urlencoded"
-					]
-		curl = pycurl.Curl()
-		curl.setopt(pycurl.TIMEOUT, 15)
-		curl.setopt(pycurl.HTTPHEADER, header)
-		curl.setopt(pycurl.URL, url)
-		#creating a string to be able to store the retrieved data in
-		curl_data = StringIO.StringIO()
-		#setting the option to write the retrieved data to the string
-		curl.setopt(pycurl.WRITEFUNCTION, curl_data.write)
-		#making it a HTTP post request
-		curl.setopt(pycurl.POST, 1)
-		#setting the data to be posted
-		curl.setopt(pycurl.POSTFIELDS, str(data))
+		headers = { "User-Agent" : "{ua}".format(ua=ua),
+					"Content-Length" : "{content_length}".format(content_length = len(data)),
+					"Connection" : "Keep-Alive",
+					"Expect" : "100-continue",
+					"Content-Type" : "application/x-www-form-urlencoded"
+					}
 		#trying to keep the script as sturdy as possible
 		try:
-			curl.perform()
-			response = curl_data.getvalue()
-			return(response)
-		except:
+			r = requests.post(url, data=data,headers=headers)
+			return(r.text)
+		except Exception as exceptio:
+			print(exceoptio)
 			pass
 		#if the request was denied, or the connection was interrupted, retrying. (up to five times)
 		fail_count = 0
-		while (curl_data.getvalue() == "") and (fail_count < 5):
+		while (r.text == "") and (fail_count < 5):
 			fail_count += 1
 			print("buffer was empty, retry time: {fails}".format(fails=fail_count))
 			try:
-				curl.perform()
+				r = requests.post(url, data=data,headers=headers)
 			except:
 				pass
 			if fail_count >= 5:
@@ -178,29 +163,19 @@ def MiniLyrics(artist,title):
 
 #function to return lyrics grabbed from lyricswikia
 def LyricWikia(artist,title):
-	curl = pycurl.Curl()
-	curl.setopt(pycurl.FOLLOWLOCATION, 1)
-	curl.setopt(curl.MAXREDIRS, 5)
-	curl.setopt(curl.CONNECTTIMEOUT, 15)
-	curl.setopt(curl.TIMEOUT, 15)
-	curl_url = u'http://lyrics.wikia.com/api.php?artist={artist}&song={title}&fmt=json'.format(artist=artist,title=title).replace(" ", "%20")
-	#first we have to query for the right url
-	curl.setopt(curl.URL, curl_url)
-	#creating a string to be able to store the retrieved data in
-	curl_data = StringIO.StringIO()
-	#setting the option to write the retrieved data to the string
-	curl.setopt(pycurl.WRITEFUNCTION, curl_data.write)
-	curl.perform()
-	#We got some bad formatted data... So we need to fix stuff :/
-	curl_return = curl_data.getvalue()
-	curl_return = curl_return.replace("\'","\"")
-	curl_return = curl_return.replace("song = ","")
-	curl_return = json.loads(curl_return)
-	if curl_return["lyrics"] != "Not found":
+	url = 'http://lyrics.wikia.com/api.php?artist={artist}&song={title}&fmt=json'.format(artist=artist,title=title).replace(" ", "%20")
+	r = requests.get(url, timeout=15)
+	#We got some bad formatted JSON data... So we need to fix stuff :/
+	returned = r.text
+	returned = returned.replace("\'","\"")
+	returned = returned.replace("song = ","")
+	returned = json.loads(returned)
+	if returned["lyrics"] != "Not found":
 		#set the url to the url we just recieved, and retrieving it
-		curl.setopt(curl.URL, str(curl_return["url"]))
-		curl.perform()
-		soup = BeautifulSoup(curl_data.getvalue())
+		r = requests.get(returned["url"], timeout=15)
+		#curl.setopt(curl.URL, str(curl_return["url"]))
+		#curl.perform()
+		soup = BeautifulSoup(r.text)
 		soup = soup.find("div", {"class": "lyricbox"})
 		[elem.extract() for elem in soup.findAll('div')]
 		[elem.replaceWith('\n') for elem in soup.findAll('br')]
